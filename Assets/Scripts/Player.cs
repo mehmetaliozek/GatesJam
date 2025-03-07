b0abd75de43849474689dc18bd388df1ed0171a7
Mehmet Ali <mehmetaliozek761@gmail.com>
2025-03-08 02:21:06 +0300

    Zıplama eklendi

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,33 @@ public class Player : MonoBehaviour, IActivator
 
     public float Speed;
 
+    public float JumpForce;
+
+    [SerializeField]
+    private Transform ground;
+    [SerializeField]
+    private LayerMask groundLayer;
+
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
+        Move();
+        Jump();
+    }
 
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(x * Speed, rb.linearVelocityY);
+    }
+
+    private void Jump()
+    {
+        bool isGround = Physics2D.OverlapPoint(ground.transform.position, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
     }
 
     public void Enable()
